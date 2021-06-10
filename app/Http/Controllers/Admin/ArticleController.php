6c94e6c6b66458456b57b050034c5c734e23642f
欧子 <ouzi7997@163.com>
2021-06-10 23:59:08 +0800

    article restful

---
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -0,0 +1,41 @@
+<?php
+
+class ArticleController extends AdminBaseController
+{
+
+    /**
+     * 获取文章列表
+     */
+    public function getList()
+    {
+
+
+    }
+
+    /**
+     * 获取文章详情
+     */
+    public function find()
+    {
+
+    }
+
+    /**
+     * 删除文章
+     */
+    public function delete()
+    {
+
+    }
+
+    /**
+     * 修改文章
+     */
+    public function update()
+    {
+
+    }
+
+
+
+}
--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -4,6 +4,24 @@ class ArticleController extends ApiBaseController
 {
 
 
+    /**
+     * 获取文章列表
+     */
+    public function getList()
+    {
+
+
+    }
+
+    /**
+     * 获取文章详情
+     */
+    public function find()
+    {
+
+    }
+
+
 
 
 
