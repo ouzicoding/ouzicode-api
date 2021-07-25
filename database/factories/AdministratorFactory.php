111206c4f921ca2ad396dd3b4ddf27bd19bc1779
欧子 <ouzi7997@163.com>
2021-07-25 23:54:47 +0800

    administrator factory

---
+++ database/factories/AdministratorFactory.php
@@ -0,0 +1,22 @@
+<?php
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdministratorFactory extends Factory
+{
+
+
+    protected $model=Administrator::class;
+
+    public function definition()
+    {
+        return [
+
+        ];
+    }
+
+
+
+
+}
