d71918b0710ecc02de73f16d62a53664780f0fa2
欧子 <ouzi7997@163.com>
2021-08-06 23:54:56 +0800

    create migration tags

--- database/migrations/2021_08_02_154620_create_tags_table.php
+++ database/migrations/2021_08_02_154620_create_tags_table.php
@@ -16,6 +16,7 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('sort')->nullable(false)->default(0);
             $table->timestamps();
         });
     }
