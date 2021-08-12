7b4d29ae4bae65679707dd82012da786ca539c7e
欧子 <ouzi7997@163.com>
2021-08-12 19:33:02 +0800

    create migration articles

--- database/migrations/2021_08_11_110746_create_articles_table.php
+++ database/migrations/2021_08_11_110746_create_articles_table.php
@@ -15,6 +15,7 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable(false)->default('');
             $table->timestamps();
         });
     }
