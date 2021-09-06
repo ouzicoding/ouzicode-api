5d128b37c35eef07c904187d2aa2eac4aa9ee4f4
欧子 <ouzi7997@163.com>
2021-09-06 23:38:59 +0800

    tags

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -3,22 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 
+use Illuminate\Http\JsonResponse;
+
 class TagController extends AdminBaseController
 {
 
 
-    public function tags()
+    public function tags(): JsonResponse
     {
 
         return response()->json();
     }
 
-    public function delete()
+    public function delete(): JsonResponse
     {
 
         return response()->json();
     }
-    public function update()
+    public function update(): JsonResponse
     {
 
         return response()->json();
