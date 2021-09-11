9b8adc8094fa080137f49c40ca68a2c275fbb20e
欧子 <ouzi7997@163.com>
2021-09-11 23:32:55 +0800

    category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,5 +5,10 @@ namespace App\Http\Controllers\Admin;
 class CategoryController extends AdminBaseController
 {
 
+    public function categories()
+    {
+
+        return response()->json();
+    }
 
 }
