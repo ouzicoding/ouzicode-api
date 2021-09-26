2fb02733325f1adbe17c97cd2ad641aaaa4459b2
欧子 <ouzi7997@163.com>
2021-09-26 23:56:00 +0800

    restful routes

--- routes/admin.php
+++ routes/admin.php
@@ -14,19 +14,19 @@ Route::post('/', function () {
 //登录
 Route::post('/login', [LoginController::class, 'login']);
 
-//文章列表
+//文章
 Route::post('article', [ArticleController::class, 'create']);
 Route::delete('articles/{id}', [ArticleController::class, 'delete']);
 Route::put('articles/{id}', [ArticleController::class, 'update']);
 Route::get('articles', [ArticleController::class, 'articles']);
 
-//标签列表
+//标签
 Route::post('tag', [TagController::class, 'create']);
 Route::delete('tags/{id}', [TagController::class, 'delete']);
 Route::put('tags/{id}', [TagController::class, 'update']);
 Route::get('tags', [TagController::class, 'tags']);
 
-//分类列表
+//分类
 Route::post('category', [CategoryController::class, 'create']);
 Route::delete('categories/{id}', [CategoryController::class, 'delete']);
 Route::put('categories/{id}', [CategoryController::class, 'update']);
