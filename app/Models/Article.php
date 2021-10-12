d8a12add58e01e2c411560080f7db35222e07d70
欧子 <ouzi7997@163.com>
2021-10-12 16:06:05 +0800

    models

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,9 +3,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Article extends Model
 {
-
 
 
 
---
+++ app/Models/Link.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Models;
+
+
+class Link extends \Illuminate\Database\Eloquent\Model
+{
+
+}
