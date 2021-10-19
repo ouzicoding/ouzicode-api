8c11646a91b4a606cd2eeafd5d6d5d4028bf0b6f
欧子 <ouzi7997@163.com>
2021-10-19 19:49:49 +0800

    category controller

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,28 +4,32 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryController extends AdminBaseController
 {
 
-    public function create()
+    public function create(Request $request)
     {
-        $insertData = [];
-        Category::insertGetId($insertData);
+        $post = $request->input();
+
+        $insertData = $input;
+        Category::create($insertData);
 
         return response()->json();
     }
 
-    public function delete($id)
+    public function delete(Request $request,$id)
     {
 
         Category::deleted($id);
         return response()->json();
     }
 
-    public function update($id)
+    public function update(Request $request,$id)
     {
-        $updateData = [];
+        $put = $request->input();
+        $updateData = $put;
         Category::where('id',$id)->update($updateData);
         return response()->json();
     }
