d410b18682c6ecb3e22c9405f5e6f7981f6e1c8c
欧子 <ouzi7997@163.com>
2021-10-28 23:28:42 +0800

    article

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -1,52 +1,50 @@
 <?php
 namespace App\Http\Controllers\Admin;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
 class ArticleController extends AdminBaseController
 {
 
+    /**
+     * 修改文章
+     */
+    public function create(Request $request)
+    {
+        $post = $request->input();
+        Article::create($post);
+        return response()->json();
+    }
+    /**
+     * 删除文章
+     */
+    public function delete($id)
+    {
+        Article::where('id',$id)->delete();
+        return response()->json();
+    }
+    /**
+     * 修改文章
+     */
+    public function update(Request $request,$id)
+    {
+        $put = $request->input();
+        Article::where('id',$id)->update($put);
+        return response()->json();
+    }
     /**
      * 获取文章列表
      */
     public function articles()
     {
-
-        return response()->json();
-
-    }
-
-    /**
-     * 获取文章详情
-     */
-    public function detail()
-    {
-        return response()->json();
-
-    }
-    /**
-     * 修改文章
-     */
-    public function create()
-    {
-        return response()->json();
+        $list = Article::where([['is_deleted'=>0]])
+            ->get();
+        return response()->json($list);
 
     }
 
-    /**
-     * 删除文章
-     */
-    public function delete()
-    {
-        return response()->json();
 
-    }
 
-    /**
-     * 修改文章
-     */
-    public function update()
-    {
-        return response()->json();
-
-    }
 
 
 
