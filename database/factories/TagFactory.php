a5c2352ce936bf8ec7249f0e9e5031da6699718d
欧子 <ouzi7997@163.com>
2021-11-26 23:54:05 +0800

    tag factory

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -22,7 +22,8 @@ class TagFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name'=>$this->faker->name(),
+            'sort'=>0,
         ];
     }
 }
