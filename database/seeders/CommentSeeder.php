0240e73f7d9691105cb66c264707de14027c3c02
欧子 <ouzi7997@163.com>
2021-11-30 23:14:10 +0800

    comment seeder

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class CommentSeeder extends Seeder
@@ -13,6 +14,7 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        Comment::factory(2)->create();
     }
 }
